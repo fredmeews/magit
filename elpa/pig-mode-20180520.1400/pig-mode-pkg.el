1cc2dce455004146551a3fe7fcf5ea880e65584f
Ed Hughes <ehughes@aperturecvo.com>
2021-03-06 15:15:52 -0500

    lisp

---
+++ elpa/pig-mode-20180520.1400/pig-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pig-mode" "20180520.1400" "Major mode for Pig files" 'nil :commit "4c6c6e1b1bb719d8adc6c47cc24665f6fe558959" :maintainer '("David A. Shamma"))
---
+++ elpa/treepy-1.0.0/treepy-autoloads.el
@@ -0,0 +1,15 @@
+;;; treepy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("treepy.el") (23538 955 215503 845000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; treepy-autoloads.el ends here
